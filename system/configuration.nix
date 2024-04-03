4cc7231f428553c4c90d7b34c979485a2e340a1f
yngtdd <yngtdd@gmail.com>
2024-04-03 14:43:58 -0400

    feat: Setup fonts

--- system/configuration.nix
+++ system/configuration.nix
@@ -56,6 +56,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    jack.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
 
@@ -74,6 +75,7 @@
       "audio"
       "networkmanager"
       "wheel"
+      "video"
     ];
   };
 
@@ -114,6 +116,13 @@
     wget
     zsh
   ];  
+
+  fonts.fontDir.enable = true;
+  fonts.packages = with pkgs; [
+    nerdfonts
+    font-awesome
+    google-fonts
+  ];
 
   nix.gc = {
     automatic = true;
