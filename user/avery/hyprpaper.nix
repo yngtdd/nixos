91cb4d08f3bee99f257ab7ebed7c94bc15f5237c
yngtdd <yngtdd@gmail.com>
2024-04-18 10:10:43 -0400

    docs: Update wallpaper README

--- user/avery/hyprpaper.nix
+++ user/avery/hyprpaper.nix
@@ -3,9 +3,10 @@
 {
   home.packages = with pkgs; [ hyprpaper ];
 
+  # 76 - kiki
   xdg.configFile."hypr/hyprpaper.conf".text = ''
-    preload = ~/nixos/wallpaper/76.jpg
-    wallpaper = HDMI-A-1,~/nixos/wallpaper/76.jpg
+    preload = ~/nixos/wallpaper/63.jpg
+    wallpaper = HDMI-A-1,~/nixos/wallpaper/63.jpg
     splash=false
   '';
 }
